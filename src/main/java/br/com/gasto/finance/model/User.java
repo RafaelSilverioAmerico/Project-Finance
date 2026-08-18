c00ca6219357eb47e4620e7a35a7c7ecaec7154c
Rafael Silveri Americo <rafael_silverio@outlook.com>
2026-08-18 17:52:14 -0300

    Criando a class User

---
+++ src/main/java/br/com/gasto/finance/model/User.java
@@ -0,0 +1,24 @@
+package br.com.gasto.finance.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
