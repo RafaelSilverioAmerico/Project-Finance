970ec81e65049f949601fd5eccd88610847bb4e2
Rafael Silveri Americo <rafael_silverio@outlook.com>
2026-08-26 15:55:32 -0300

    Criando AuthController

---
+++ src/main/java/br/com/gasto/finance/controller/AuthController.java
@@ -0,0 +1,31 @@
+package br.com.gasto.finance.controller;
+
+import br.com.gasto.finance.dto.AuthResponse;
+import br.com.gasto.finance.dto.LoginRequest;
+import br.com.gasto.finance.dto.RegisterRequest;
+import br.com.gasto.finance.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid  @RequestBody RegisterRequest req) {
+        return ResponseEntity.ok(authService.register(req));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid  @RequestBody LoginRequest req) {
+        return ResponseEntity.ok(authService.login(req));
+    }
+}
