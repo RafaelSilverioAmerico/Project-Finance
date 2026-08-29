cf9203f8747605130e083bef2fc24cbae58f1fbf
Rafael Silveri Americo <rafael_silverio@outlook.com>
2026-08-29 12:02:34 -0300

    Exibindo resumo mensal e atualizacao em tempo real

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,11 +1,11 @@
 if (!Auth.estaAutenticado()) {
-    window.location.href ='index.html';
+    window.location.href = 'index.html';
 }
 
 const usuario = Auth.getUser();
 document.getElementById('user-name').textContent = usuario.nome;
 
-document.getElementById('btn-logout').addEventListener('click',() => {
+document.getElementById('btn-logout').addEventListener('click', () => {
     Auth.limpar();
     window.location.href = 'index.html';
 });
@@ -15,6 +15,7 @@ async function carregarCategorias() {
     const select = document.getElementById('select-categoria');
     select.innerHTML = categorias.map(c => `<option value="${c.id}">${c.nome}</option>`).join('');
 }
+
 carregarCategorias();
 
 document.getElementById('form-gasto').addEventListener("submit", async (e) => {
@@ -35,15 +36,41 @@ document.getElementById('form-gasto').addEventListener("submit", async (e) => {
 async function carregarGastos() {
     const hoje = new Date();
     const inicio = `${hoje.getFullYear()}-${String(hoje.getMonth() + 1).padStart(2, '0')}-01`;
-    const fim  = `${hoje.getFullYear()}-${String(hoje.getMonth() + 1).padStart(2, '0')}-31`;
+    const fim = `${hoje.getFullYear()}-${String(hoje.getMonth() + 1).padStart(2, '0')}-31`;
 
     const gastos = await Api.listarGastos(inicio, fim);
     const container = document.getElementById('lista-gastos');
 
-    container.innerHTML = gastos.map(g =>`
+    container.innerHTML = gastos.map(g => `
     <div>
         <strong>${g.descricao}</strong> - R$ ${g.valor} - ${g.categoriaNome} (${g.data})
     </div>
     `).join('');
 }
+
 carregarGastos();
+
+async function carregarResumo() {
+    const hoje = new Date();
+    const resumo = await Api.resumoMensal(hoje.getFullYear(), hoje.getMonth() + 1);
+    document.getElementById('total-mes').textContent = resumo.total.toFixed(2).replace('.', ',');
+}
+
+carregarResumo();
+
+const socket = new SockJS(`http://localhost:8080/ws`);
+const client = new StompJs.Client({
+    webSocketFactory: () => socket,
+});
+
+client.onConnect = () => {
+    client.subscribe(`/topic/gastos/${usuario.id}`, (mensagem) => {
+        const evento = JSON.parse(mensagem.body);
+        console.log('Evento recebido:', evento);
+
+        carregarGastos();
+        carregarResumo();
+    });
+};
+
+client.activate();
